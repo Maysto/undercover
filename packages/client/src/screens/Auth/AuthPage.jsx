388d8a7fe116493c19321b971b61c839f8aba912
Garnier <corentingarnier06@gmail.com>
2022-04-18 17:56:43 +0200

    merde

---
+++ packages/client/src/screens/Auth/AuthPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AuthPage = () => {
+    return (
+        <div>
+            <h1>AuthPage</h1>
+            <Button color="danger">Danger!</Button>
+        </div>
+    );
+};
+
+export default AuthPage;
